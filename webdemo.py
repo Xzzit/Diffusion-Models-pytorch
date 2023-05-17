4f5f3fdc13555ff9214e2be4604f0c141779c53c
Xzzit <897177017@qq.com>
2023-05-17 16:46:32 +0900

    delete save img in training

--- webdemo.py
+++ webdemo.py
@@ -489,23 +489,6 @@ def sample(model, image_size, batch_size=16, channels=3):
     return p_sample_loop(model, shape=(batch_size, channels, image_size, image_size))
 
 
-
-from pathlib import Path
-
-def num_to_groups(num, divisor):
-    groups = num // divisor
-    remainder = num % divisor
-    arr = [divisor] * groups
-    if remainder > 0:
-        arr.append(remainder)
-    return arr
-
-results_folder = Path("./results")
-results_folder.mkdir(exist_ok = True)
-save_and_sample_every = 1000
-
-
-
 from torch.optim import Adam
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -541,16 +524,6 @@ for epoch in range(epochs):
 
         loss.backward()
         optimizer.step()
-
-      # save generated images
-        if step != 0 and step % save_and_sample_every == 0:
-            milestone = step // save_and_sample_every
-            batches = num_to_groups(4, batch_size)
-            all_images_list = list(map(lambda n: sample(model, batch_size=n, channels=channels), batches))
-            all_images = torch.cat(all_images_list, dim=0)
-            all_images = (all_images + 1) * 0.5
-            save_image(all_images, str(results_folder / f'sample-{milestone}.png'), nrow = 6)
-
 
 
 # sample 64 images
